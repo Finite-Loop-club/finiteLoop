0f17e77646a3287da3e56786b1fcc2321172903b
anjuman raj <ayusmann23@gmail.com>
2022-05-28 16:02:22 +0530

    hamburger icons added in navbar

--- src/Components/navbars/AuthNavbar.js
+++ src/Components/navbars/AuthNavbar.js
@@ -6,6 +6,8 @@ import { MdOutlineEmojiEvents, } from 'react-icons/md'
 import { RiGalleryLine, } from 'react-icons/ri'
 import { FaTeamspeak, } from 'react-icons/fa'
 import { FiFacebook, FiInstagram, FiLinkedin } from 'react-icons/fi'
+import { CgMenuRight} from 'react-icons/cg'
+import { GrClose} from 'react-icons/gr'
 
 
 
@@ -71,17 +73,18 @@ function AuthNavbar() {
                             Finite Loop
                         </Link>
                         <button
-                            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                            className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none transition-all duration-150 "
                             type="button"
                             onClick={() => setNavbarOpen(!navbarOpen)}
                         >
-                            <i className="text-white fas fa-bars"></i>
+                            {!navbarOpen ? <CgMenuRight className='inline text-slate-50 mr-2 text-lg' /> : <GrClose className='inline text-slate-50 mr-2 text-lg' />}
+                            
                         </button>
                     </div>
                     <div
                         className={
-                            "lg:flex flex-grow items-center bg-white lg:bg-opacity-0 lg:shadow-none" +
-                            (navbarOpen ? " block rounded shadow-lg" : " hidden")
+                            "lg:flex flex-grow items-center bg-slate-800  lg:bg-opacity-0 lg:shadow-none" +
+                            (navbarOpen ? " block rounded shadow-lg " : " hidden")
                         }
                         id="example-navbar-warning"
                     >
@@ -91,7 +94,7 @@ function AuthNavbar() {
                                     <>
                                         <li key={key} className="flex items-center">
                                             <Link
-                                                className="lg:text-white lg:hover:text-slate-200 text-slate-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                                className="text-white lg:hover:text-slate-200  px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
                                                 to={menu.link}
                                             >
                                                 {menu.icons}
@@ -114,12 +117,12 @@ function AuthNavbar() {
                                     <>
                                         <li key={key} className="flex items-center">
                                             <a
-                                                className="lg:text-white lg:hover:text-slate-200 text-slate-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                                                className="text-white lg:hover:text-slate-200  px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
                                                 href={index.link}
                                                 target="_blank"
                                             >
                                                 {index.icons}
-                                                <span className="lg:hidden inline-block ml-2">{index.name}</span>
+                                                <span className="lg:hidden inline-block ">{index.name}</span>
                                             </a>
                                         </li>
 
@@ -133,7 +136,8 @@ function AuthNavbar() {
                                         className="bg-white text-slate-700 active:bg-slate-50 text-md font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                                         type="button"
                                     >
-                                        <FaTeamspeak className='inline text-slate-900 mr-2 text-lg' /> Login
+                                        <FaTeamspeak className='inline text-slate-900 mr-2 text-lg' /> 
+                                        Profile
                                     </button>
                                 </Link>
                             </li>
