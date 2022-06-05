032456af920231fd864c0bd7bf77f05be008aa38
anjuman raj <ayusmann23@gmail.com>
2022-06-06 00:01:34 +0530

    dashboard members details iterated

--- src/Components/DashboardComp/cards/CardTable.js
+++ src/Components/DashboardComp/cards/CardTable.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import src from '../../../assets/member/core/johnWick.png'
-// import { firestore } from "../../../firebase";
+import { db } from "../../../firebase";
+import { doc, getDoc, collection, getDocs } from "firebase/firestore";
 // components
 
 import TableDropdown from "../Dropdowns/TableDropdown";
@@ -9,142 +10,159 @@ import { data } from "autoprefixer";
 
 export default function CardTable({ color }) {
 
-
-  // useEffect(() => {
-  //   const fetchMember = async () => {
-  //     const doc = await firestore
-  //       .collection("members")
-  //       .doc("4NM19CS050")
-  //       .get();
-
-  //     const data = doc.data();
-  //     console.log("data " + data);
-  //     if (!data) {
-  //       // document didn't exist
-  //       console.log("hit null");
-  //       setCart(null)
-  //     } else {
-  //       console.log("hit");
-  //       setCart(data.cart);
-  //     }
-  //     console.log("cart " + cart);
-  //   }
-
-  //   fetchCart();
-  // }, []);
+  // const [data,setData] = useState([{}])
+  const [members, setMembers] = useState([]);
+  // const temp = [];
+  const fetchMembers = async () => {
 
 
 
-return (
-  <>
-    <div
-      className={
-        "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
-        (color === "light" ? "bg-white" : "bg-sky-900 text-white")
-      }
-    >
-      <div className="rounded-t mb-0 px-4 py-3 border-0">
-        <div className="flex flex-wrap items-center">
-          <div className="relative w-full px-4 max-w-full flex-grow flex-1">
-            <h3
-              className={
-                "font-semibold text-lg " +
-                (color === "light" ? "text-slate-700" : "text-white")
-              }
-            >
-              Members Details
-            </h3>
+    const querySnapshot = await getDocs(collection(db, "members"));
+    querySnapshot.forEach((doc) => {
+      // console.log(doc.id, " => ", doc.data().name);
+      setMembers((e)=>[...e,doc.data()])
+    });
+
+
+    // setMembers(temp)
+    // console.log(members)
+    // return members
+  }
+  // const temp=[];
+  // const temp=members.map((data,id)=>id==2)
+  console.log(members)
+
+
+  useEffect(() => {
+    // setData(fetchMembers())
+    fetchMembers()
+    // setMembers(fetchMembers());
+
+  }, [])
+  // console.log(members);
+
+
+  return (
+    <>
+      <div
+        className={
+          "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
+          (color === "light" ? "bg-white" : "bg-sky-900 text-white")
+        }
+      >
+        <div className="rounded-t mb-0 px-4 py-3 border-0">
+          <div className="flex flex-wrap items-center">
+            <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+              <h3
+                className={
+                  "font-semibold text-lg " +
+                  (color === "light" ? "text-slate-700" : "text-white")
+                }
+              >
+                Members Details
+              </h3>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="block w-full overflow-x-auto">
-        {/* Projects table */}
-        <table className="items-center w-full bg-transparent border-collapse">
-          <thead>
-            <tr>
-              <th
-                className={
-                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                  (color === "light"
-                    ? "bg-slate-50 text-slate-500 border-slate-100"
-                    : "bg-sky-800 text-sky-300 border-sky-700")
-                }
-              >
-                Name
-              </th>
-              <th
-                className={
-                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                  (color === "light"
-                    ? "bg-slate-50 text-slate-500 border-slate-100"
-                    : "bg-sky-800 text-sky-300 border-sky-700")
-                }
-              >
-                USN
-              </th>
-              <th
-                className={
-                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                  (color === "light"
-                    ? "bg-slate-50 text-slate-500 border-slate-100"
-                    : "bg-sky-800 text-sky-300 border-sky-700")
-                }
-              >
-                Phone
-              </th>
-              <th
-                className={
-                  "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
-                  (color === "light"
-                    ? "bg-slate-50 text-slate-500 border-slate-100"
-                    : "bg-sky-800 text-sky-300 border-sky-700")
-                }
-              >
-                Email
-              </th>
-
-            </tr>
-          </thead>
-          <tbody>
-
-            <tr>
-              <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
-                <img
-                  src={src}
-                  className="h-12 w-12 bg-white rounded-full border"
-                  alt="..."
-                ></img>{" "}
-                <span
+        <div className="block w-full overflow-x-auto">
+          {/* Projects table */}
+          <table className="items-center w-full bg-transparent border-collapse">
+            <thead>
+              <tr>
+                <th
                   className={
-                    "ml-3 font-bold " +
-                    +(color === "light" ? "text-slate-600" : "text-white")
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-slate-50 text-slate-500 border-slate-100"
+                      : "bg-sky-800 text-sky-300 border-sky-700")
                   }
                 >
-                  Argon Design System
-                </span>
-              </th>
-              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                $2,500 USD
-              </td>
-              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                <i className="fas fa-circle text-orange-500 mr-2"></i> pending
-              </td>
+                  Name
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-slate-50 text-slate-500 border-slate-100"
+                      : "bg-sky-800 text-sky-300 border-sky-700")
+                  }
+                >
+                  USN
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-slate-50 text-slate-500 border-slate-100"
+                      : "bg-sky-800 text-sky-300 border-sky-700")
+                  }
+                >
+                  Phone
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                    (color === "light"
+                      ? "bg-slate-50 text-slate-500 border-slate-100"
+                      : "bg-sky-800 text-sky-300 border-sky-700")
+                  }
+                >
+                  Email
+                </th>
 
-              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                <i className="fas fa-circle text-orange-500 mr-2"></i> pending
-              </td>
+              </tr>
+            </thead>
 
-              <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
-                <TableDropdown />
-              </td>
-            </tr>
 
-          </tbody>
-        </table>
+
+            <tbody>
+
+               {members.map((index, id)=>{
+                return(
+                  <tr key={id} >
+                    <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                      <img
+                        src={src}
+                        className="h-12 w-12 bg-white rounded-full border"
+                        alt="..."
+                      ></img>{" "}
+                      <span
+                        className={
+                          "ml-3 font-bold " +
+                          +(color === "light" ? "text-slate-600" : "text-white")
+                        }
+                      >
+                        {index.name}
+                      </span>
+                    </th>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {index.usn}
+                    </td>
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                       {index.phone}
+                    </td>
+
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                      {index.email}
+                    </td>
+
+                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-right">
+                      <TableDropdown />
+                    </td>
+                  </tr>
+                )
+              })} 
+
+
+
+
+
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
 }
 
 CardTable.defaultProps = {
