581d10536224a13ba95dab0943be0fb845566ef0
anjuman raj <ayusmann23@gmail.com>
2022-07-04 21:49:35 +0530

    finiteloop g form

--- src/views/registration/GoogleForm.js
+++ src/views/registration/GoogleForm.js
@@ -4,7 +4,7 @@ function GoogleForm() {
     return (
         <>
         <div className='w-fit mx-auto mt-4' >
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScXfHaOcKDiwN82xQbHaEFFFuE0tWiGjZH0VmTjMCO1mOq9YA/viewform?embedded=true" width="640" height="1700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSejr6jCmppsRdhE8EPF_ARrH14qaDFjAEFykpq-c-G6FxrLEA/viewform" width="640" height="1700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
         </>
     )
