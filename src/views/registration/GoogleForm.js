cf7a20f896fd94779185d05031be25fa3d498fc0
anjuman raj <ayusmann23@gmail.com>
2022-07-04 22:06:33 +0530

    size adjusted

--- src/views/registration/GoogleForm.js
+++ src/views/registration/GoogleForm.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 function GoogleForm() {
+
+
     return (
         <>
-        <div className='w-fit mx-auto mt-4' >
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSejr6jCmppsRdhE8EPF_ARrH14qaDFjAEFykpq-c-G6FxrLEA/viewform" width="640" height="1700" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <div className='w-screen mt-4 mb-8 ' >
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSejr6jCmppsRdhE8EPF_ARrH14qaDFjAEFykpq-c-G6FxrLEA/viewform" className='w-11/12 md:w-1/2 mx-auto bg-none ' height="3175" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
         </>
     )
