7114a266c272240573f65194abab6362cb123258
anjuman raj <ayusmann23@gmail.com>
2022-07-04 20:56:25 +0530

    event data added

--- src/views/event/eventData.js
+++ src/views/event/eventData.js
@@ -5,10 +5,10 @@ export const eventData = [
     attended:"60+ Teams",
     type:"Cyber Security",
     image: "/events/cyhack/picture1.png",
-    oragnizer: "Sanath R Pai, Shravya S Rao , Shrilaxmi Pai ",
+    oragnizer: "Sanath R Pai, Shravya S Rao , Shrilakshmi Pai N",
     desc:
       "Information Security competition that challenges the contestants to solve a variety of tasks ranging from a scavenger hunt on Wikipedia to basic programming exercises, to hack their way into a server to steal the data.",
-    year:"2020-21"
+    year: "2020-21"
   },
  
   {
@@ -20,20 +20,127 @@ export const eventData = [
     oragnizer: "Finite Loop Team",
     desc:
       "Finite Loop club, NMAMIT, Nitte, successfully conducted a gaming event on 11th March, 2021. It was a fun and a lively event. More than 150 students competed to obtain the title of Ultimate Champions. The event consisted of three games, which were Call of Duty, NFS, eFootballPes",
-      year:"2020-21"
-    },
-    {
-      name: "CODE INNOVATION SERIES",
-      date: "28-30 October, 2020",
-      attended:"80+ Teams",
-      type:"Open source Hackathon",
-      image: "/events/code_innovation/picture2.png",
-      oragnizer: "Finite Loop Team",
-      desc:
-      "Finite Loop Club under the Centre for Student Innovation (CFSI) in association with the New Age Innovation Network (NAIN), KITS, Dept. of Electronics, IT, BT and S&T, Govt. of Karnataka organized “CODE INNOVATION SERIES - NMAMIT” hosted by IncubateIND under the GitHub Campus Program in collaboration with our college, and thus was a part of India's largest open source Hackathon.",
-      year:"2020-21"
-    },
- 
-
-  
+    year: "2020-21"
+  },
+  {
+    name: "CODE INNOVATION SERIES",
+    date: "28-30 October, 2020",
+    attended:"80+ Teams",
+    type:"Open source Hackathon",
+    image: "/events/code_innovation/Picture2.png",
+    oragnizer: "Finite Loop Team",
+    desc:
+      "Finite Loop Club under the Centre for Student Innovation (CFSI) in association with the New Age Innovation Network (NAIN), KITS, Dept. of Electronics, IT, BT and S&T, Govt. of Karnataka organized “CODE INNOVATION SERIES – NMAMIT” hosted by IncubateIND under the GitHub Campus Program in collaboration with our college, and thus was a part of India’s largest open source Hackathon.",
+    year: "2020-21"
+  },
+  {
+    name: " WAR ZONE",
+    date: "26/11/2021",
+    attended:"10+ Teams",
+    type:"Gaming",
+    image: "/events/warzone/warzone.jpeg",
+    oragnizer: "Thejas Kumar, Dhanish S Suvarna",
+    desc:
+      "Call of Duty (COD) was played by the registered teams based on the maps, FiringRange, Standoff, Summit, Nuketown. Teams were made to compete against each other based on toss and after a great fight, Team DPH could battle through excel! Winners: Shravan P, Aravinda Krishna U, Rathan B, M Ankur Baliga, Achal Rai.",
+    year: "2021-22"
+  },
+  {
+    name: "Web Appilcation Hacking and Penetration Testing",
+    date: "16/12/2021",
+    attended:"40+ Teams",
+    type:"Cyber Security",
+    image: "/events/hacking/hacking.jpeg",
+    oragnizer: "Abdeali",
+    desc:
+      "This workshop was conducted by Abdeali, 3rd CSE, NMAMIT. It had a positive impact among the participants and gave the newbies the required knowledge and resources to kickstart their journey in the field of Cyber Security.",
+    year: "2021-22"
+  },
+  {
+    name: "Naari Tattva",
+    date: "13/03/2021",
+    attended:"100+ Participants",
+    type:"Seminar",
+    image: "/events/naritatva/picture.png",
+    oragnizer: "Finite Loop Team",
+    desc:
+      "Finite Loop NMAMIT, Nitte, organized an event, 'Naari Tattva', on 13th of March, 2021. This event glorified the achievements of women in various fields. The session consisted of Inspirational talk by women achievers, Stand-up comedy, Poetry, Shayari, narrating short stories and The grand quiz.",
+    year: "2020-21"
+  },
+  {
+    name: "DEBUNKER 101",
+    date: "26/04/2021",
+    attended:"150+ Participants",
+    type:"Seminar",
+    image: "/events/debunker/picture1.png",
+    oragnizer: "Sanath R Pai, Shravya S Rao, Rahul S",
+    desc:
+      "Finite Loop Club-NMAMIT in association with the Dept. of Electrical and Electronics Engineering, conducted the virtual debugging workshop Debunker 101 on 26th April 2021 from 2-5 pm. The workshop was conducted to provide placement related tips on debugging and coding in C/C++ for pre-final year students of the EEE dept.",
+    year: "2020-21"
+  },
+  {
+    name: "Importance of Web in the era of AI",
+    date: "10/10/2020",
+    attended:"200+ Participants",
+    type:"Seminar",
+    image: "/events/imp_web_ai_era/crp_pic.png",
+    oragnizer: "Finite Loop Team",
+    desc:
+      "Dr. Gokul S Krishnan in his guest lecture, spoke about the evolution of the web. He discussed various versions through which the web has evolved. He also went on to talk about Machine Learning, Internet of Things, and the role of web and web services. The two hours of the session provided glimpses of the research direction of various trending technologies.",
+    year: "2020-21"
+  },
+  {
+    name: "A hands on session on DevOps",
+    date: "24-25th February 2018",
+    attended:"30+ Participants",
+    type:"Workshop",
+    image: "/events/devops/picture1.png",
+    oragnizer: "Finite Loop Team",
+    desc:
+      "Finite Loop had successfully conducted a two-day workshop on “A hands on session on DevOps“. Students from various semesters actively took part in the workshop. The session started off with Introduction to DevOps. The session then continued with Git, a version control system for tracking changes in computer files and coordination. The usage of GitHub and remote connection to GitHub was taught in the session. The second day began with the second session of DevOps workshop. The testing of code (i.e. project) by an automated build (Travis CI) was taught in the session. Docker, a computer program performing OS level virtualization was introduced. Dockerizing a project was taught and we deployed our own web server using docker and Django, a high-level python framework. This marked the end of the 2-day workshop on “A hands on session on DevOps”",
+    year: "2017-20"
+  },
+  {
+    name: "Screening Test",
+    date: "12/01/2018",
+    attended:"40+ Participants",
+    type:"Workshop",
+    image: "/events/screening_test/Picture1.jpg",
+    oragnizer: "Finite Loop Team",
+    desc:
+      "A screening test was conducted for those who wished to be a part of this club. In the first round, 20 objective type questions on C and C++ were to be answered by the students. Students from various branches took part in it. For the second round, the short listed students were asked to solve two questions, one of them being a problem statement, and another, a web development question. 9 contestants were selected as the new members of Finite Loop, based on their style, creativity and problem solving ability.",
+    year: "2017-20"
+  },  
+  {
+    name: "Android App development",
+    date: "28-29th October 2017",
+    attended:"30+ Participants",
+    type:"Android Development",
+    image: "/events/android_dev/picture3.png",
+    oragnizer: "Finite Loop Team",
+    desc:
+      "Finite Loop in association with Centre For Student Innovation(CFSI), had conducted a 2-day Workshop on “Android App Development”. The students were taught the basics of Android, its life cycles, coding with Android studio. The students were able to develop a small scale android application at the end of the sessions.",
+    year: "2017-20"
+  },
+  {
+    name: "Introduction to Web Development",
+    date: "16-17th September 2017",
+    attended:"60+ Participants",
+    type:"Web Development",
+    image: "/events/intro_to_web/Picture3.png",
+    oragnizer: "Finite Loop Team",
+    desc:
+      "Finite loop in association with Centre For Student Innovation(CFSI), had conducted a 2-day Workshop on “Introduction to Web Designing”. The sessions covered topics on basic Html, CSS and JavaScript.",
+    year: "2017-20"
+  },
+  {
+    name: "Backend Development using PHP",
+    date: "23-24th September 2017",
+    attended:"50+ Participants",
+    type:"Web Development",
+    image: "/events/backend_dev/Picture2.jpg",
+    oragnizer: "Finite Loop Team",
+    desc:
+      "Finite Loop in association with Centre For Student Innovation(CFSI), has conducted a 2-day Workshop on “Backend Development using PHP”. The sessions covered topics on server side programming concepts using PHP.",
+    year: "2017-20"
+  },
 ];
