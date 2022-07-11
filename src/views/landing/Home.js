cfa361cf3307395eea01937971ccd5c41813de5f
anjuman raj <ayusmann23@gmail.com>
2022-07-11 22:18:12 +0530

    trending set hidden block to for phone

--- src/views/landing/Home.js
+++ src/views/landing/Home.js
@@ -308,7 +308,7 @@ function Home() {
                             <div className="flex flex-wrap items-center ">
                                 <div className="w-full md:w-6/12 px-4 mx-auto mt-32">
                                     <div className="justify-center flex flex-wrap relative">
-                                        <div className="my-4 w-full lg:w-6/12 px-4 hidden md:block">
+                                        <div className="my-4 w-full lg:w-6/12 px-4 block">
 
                                             <div className="bg-red-600 shadow-lg rounded-lg text-center p-8">
                                                 <img
